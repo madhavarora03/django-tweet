376d2631fd958ad44e310fdcaf56986cd3088ec8
madhav <eragon.cube@outlook.com>
2025-02-25 00:27:38 +0530

    Add Tweet model and form.

--- tweetheadq/tweet/models.py
+++ tweetheadq/tweet/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    thumbnail = models.ImageField(upload_to='images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:10]}{"..." if len(self.text) > 10 else ""}"
