e358cea83fa46dd19e4b4c776ebad68511fb7d39
madhav <eragon.cube@outlook.com>
2025-02-24 23:14:08 +0530

    Install tailwind in django

--- tweetheadq/tweetheadq/settings.py
+++ tweetheadq/tweetheadq/settings.py
@@ -36,7 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -46,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'tweetheadq.urls'
--- tweetheadq/tweetheadq/urls.py
+++ tweetheadq/tweetheadq/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+
+                  path("__reload__/", include("django_browser_reload.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
