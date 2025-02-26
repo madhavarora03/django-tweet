2e0e9fe4f251439e6ff4f9c62e45e8e5ab800cae
madhav <eragon.cube@outlook.com>
2025-02-26 12:38:53 +0530

    Create gunicorn config for deployment.

--- tweetheadq/settings.py
+++ tweetheadq/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import dj_database_url
 import dotenv
 
-dotenv.load_dotenv("../.env")
+dotenv.load_dotenv(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
